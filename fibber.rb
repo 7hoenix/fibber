61e167bb2b65c8d56e3da7edee7711aa7b346221
philmphoenix <phillip.matthew.phoenix@gmail.com>
2015-05-23 14:40:33 -0600

    Completed project

--- fibber.rb
+++ fibber.rb
@@ -5,9 +5,22 @@ class Fibber
   #
   # end
 
+  def input
+    puts "How many numbers do you want the sequence to be?"
+    number_requested = gets.chomp.to_i
+    puts "Which format would you like to see it in (options are 1, 2 or 3)?"
+    output_requested = gets.chomp.to_i
+    sequence(number_requested, output_requested)
+  end
+
+  def sequence(number_requested, output_requested)
+    seq = calculate(number_requested)
+    (display(seq, number_requested, output_requested))
+  end
+
   def calculate(number_requested)
+    return nil if number_requested == 0
     pattern = []
-    # binding_pry
     number_requested.times do |num|
       if num == 0
         pattern[num] = num
@@ -17,15 +30,44 @@ class Fibber
         pattern[num] = pattern[num-1].to_i + pattern[num-2].to_i
       end
     end
-    display(pattern, number_requested)
     pattern
   end
 
-  def display(pattern, number_requested)
-    pattern[number_requested]
+  def display(sequence, number_requested, output_requested)
+    if output_requested == 1
+      number_requested.times do |num|
+        new_num = num + 1
+        if new_num % 5 == 0
+          puts "#{sequence[num]} "
+        else
+          print "#{sequence[num]} "
+        end
+      end
+    elsif output_requested == 2
+      number_requested.times do |num|
+        new_num = num + 1
+        centered = sequence[num].to_s.center(4)
+        if new_num % 5 == 0
+          puts "#{centered} "
+        else
+          print "#{centered} "
+        end
+      end
+    elsif output_requested == 3
+      number_requested.times do |num|
+        new_num = num + 1
+        padding = sequence.last.to_s.length.to_i + 1
+        centered = sequence[num].to_s.center(padding)
+        if new_num % 5 == 0
+          puts "#{centered} "
+        else
+          print "#{centered} "
+        end
+      end
+
+    end
   end
 end
 
 fibber = Fibber.new
-fibber.calculate(7)
-
+fibber.input
--- fibber_test.rb
+++ fibber_test.rb
@@ -1,5 +1,9 @@
-require "minitest"
-require "./fibber"
+require "simplecov"
+SimpleCov.start
+
+require "minitest/autorun"
+require_relative "fibber"
+
 
 class FibberText < Minitest::Test
   def test_is_fibber
@@ -11,6 +15,28 @@ class FibberText < Minitest::Test
     number_requested = 7
     calculations = fibber.calculate(number_requested)
     assert_equal [0,1,1,2,3,5,8], calculations
+
+    number_requested = 3
+    calculations = fibber.calculate(number_requested)
+    assert_equal [0, 1, 1], calculations
+
+    number_requested = 5
+    calculations = fibber.calculate(number_requested)
+    assert_equal [0, 1, 1, 2, 3], calculations
+  end
+
+  def test_calculate_returns_nil_when_requested_is_zero
+      fibber = Fibber.new
+      number_requested = 0
+      calculations = fibber.calculate(number_requested)
+      assert_equal nil, calculations
+  end
+
+  def test_calculate_returns_zero_when_requested_is_one
+    fibber = Fibber.new
+    number_requested = 1
+    calculations = fibber.calculate(number_requested)
+    assert_equal [0], calculations
   end
 
   def test_display_prints_out_the_sequence_with_the_correct_format
